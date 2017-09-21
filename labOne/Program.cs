8cc04e7dc298d884d7ae86395cd885e9d05f6698
bjm37 <bjm37@calvin.edu>
2017-09-20 20:41:19 -0400

    first commit

--- labOne/Program.cs
+++ labOne/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labOne
 {
@@ -10,6 +6,30 @@ namespace labOne
     {
         static void Main(string[] args)
         {
+            do
+            {
+                //Ask user for room measurements
+                Console.WriteLine("Let's measure the dimensions of this room! Please enter the length of the room:");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of this room:");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the height of this room:");
+                double height = double.Parse(Console.ReadLine());
+
+                //Calculate dimensions
+                double area = (length * width);
+                double perimeter = 2 * (length + width);
+                double volume = (length * width * height);
+
+                //Print dimensions to the console
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.WriteLine("Volume: " + volume);
+
+                Console.WriteLine("Do you want to continue? Y/N");
+            } while (Console.ReadLine() == "Y");
         }
     }
 }
